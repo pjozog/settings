2b0b773d066f42410074cd7414fb61c24d47bd73
Paul Ozog <devnull@localhost>
2011-03-29 20:26:37 -0400

    changed some settings

--- emacs-stuff/.emacs.d/init.el
+++ emacs-stuff/.emacs.d/init.el
@@ -12,7 +12,7 @@
  '(inhibit-default-init t)
  '(inhibit-startup-screen t)
  '(jabber-account-list (quote (("pjozog@gmail.com" (:network-server . "talk.google.com") (:connection-type . ssl)))))
- '(org-hide-leading-stars t)
+ '(org-hide-leading-stars nil)
  '(uniquify-buffer-name-style (quote forward) nil (uniquify))
  '(vc-follow-symlinks nil)
  '(visible-bell t))
@@ -96,6 +96,12 @@
 (defun find-file-this ()
   (interactive)
    (find-file buffer-file-name))
+
+(defun command-line-diff (switch)
+  (let ((file1 (pop command-line-args-left))
+	(file2 (pop command-line-args-left)))
+    (ediff file1 file2)))
+(add-to-list 'command-switch-alist '("-diff" . command-line-diff))
 
 ;; --------------------------------------------------
 ;; Packages / Minor modes / Keybindings
