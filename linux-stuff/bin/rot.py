6cff9fc248b92e4f8970f3426e66bb82cfd325c2
Paul Ozog <devnull@localhost>
2011-05-23 21:24:05 -0400

    Added stupid though perhaps slightly useful rot.py

---
+++ linux-stuff/bin/rot.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+
+def rot(string, num):
+    return "".join(chr(ord(c) + num) for c in string)
+
+def flip(string, num):
+    return "".join(chr(255 - ord(c)) for c in string)
+
+FUNC = rot
+
+if __name__ == '__main__':
+
+    if len(sys.argv) == 1:
+
+        print " ".join([sys.argv[0], 'file', 'num'])
+        sys.exit(1)
+
+    if len(sys.argv) == 2:
+
+        for l in sys.stdin.readlines():
+            print FUNC(l.strip(), int(sys.argv[1]))
+
+    else:
+        print FUNC(sys.argv[1], int(sys.argv[2]))
+
