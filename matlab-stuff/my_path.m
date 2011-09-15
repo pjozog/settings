d3cb0f34e666e45d6a96e6cacb260fd21b3d40b0
Paul Ozog <devnull@localhost>
2011-09-15 10:50:06 -0400

    Added reading assignment 3

--- matlab-stuff/my_path.m
+++ matlab-stuff/my_path.m
@@ -1,4 +1,5 @@
 run('~/Documents/MATLAB/vlfeat-0.9.13/toolbox/vl_setup.m');
-addpath(genpath('~/perls/matlab'));
+%addpath(genpath('~/perls/matlab'));
 addpath('~/Documents/MATLAB/sift');
-addpath('~/sba-eval/matlab/utils');
+addpath('~/Documents/MATLAB/toolbox_calib');
+% addpath('~/sba-eval/matlab/utils');
