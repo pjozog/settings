c3cc3c9d2b33f6cbf0565f0672cc305c19695a1d
Paul Ozog <devnull@localhost>
2011-09-20 10:52:10 -0400

    Added 492/568 aliases.  Made emacs slightly less transparent.  Added terminator shortcut for openbox.

--- emacs-stuff/.emacs.d/init.el
+++ emacs-stuff/.emacs.d/init.el
@@ -172,8 +172,8 @@
 (column-number-mode 1)
 
 ;; Transparency looks sweet
-(set-frame-parameter (selected-frame) 'alpha '(85 85))
-(add-to-list 'default-frame-alist '(alpha 85 85))
+(set-frame-parameter (selected-frame) 'alpha '(90 90))
+(add-to-list 'default-frame-alist '(alpha 90 90))
 
 ;; Woman is pretty cool
 (setq woman-use-own-frame nil)
