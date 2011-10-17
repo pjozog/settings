0453cca9c96d27dc702378e22c926658770e7dab
Paul Ozog <devnull@localhost>
2011-10-17 12:44:48 -0400

    Fixed background color issue in -nw mode. Disabled muting with Print Screen key because it doesn't work in ubuntu 11.10...

--- emacs-stuff/.emacs.d/init.el
+++ emacs-stuff/.emacs.d/init.el
@@ -327,7 +327,6 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "#130015" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(cursor ((t (:background "red"))))
  '(dired-directory ((t (:inherit font-lock-builtin-face))))
  '(dired-symlink ((t (:inherit font-lock-comment-face))))
