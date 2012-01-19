5d1727195b1c16fc275266a91697d8193ccd93a8
Paul Ozog <devnull@localhost>
2012-01-19 00:01:04 -0500

    Need to enable some weird chrome option on my laptop

--- awesome-stuff/awesome/rc.lua
+++ awesome-stuff/awesome/rc.lua
@@ -63,7 +63,7 @@ myawesomemenu = {
 }
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "Chrome", "google-chrome" },
+                                    { "Chrome", "google-chrome --allow-outdated-plugins" },
                                     { "Nautilus", "nautilus --no-desktop" },
                                     { "Terminal", terminal },
 				 }
@@ -203,7 +203,7 @@ globalkeys = awful.util.table.join(
    awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
    awful.key({ modkey, "Shift"   }, "y", function () awful.util.spawn("mypython") end),
    awful.key({ modkey, "Shift"   }, "f", function () awful.util.spawn("firefox") end),
-   awful.key({ modkey, "Shift"   }, "g", function () awful.util.spawn("google-chrome") end),
+   awful.key({ modkey, "Shift"   }, "g", function () awful.util.spawn("google-chrome --allow-outdated-plugins") end),
    awful.key({ modkey, "Shift"   }, "e", function () awful.util.spawn("emacs --debug-init") end),
    awful.key({ modkey, "Shift"   }, "m", function () awful.util.spawn("nautilus --no-desktop") end),
    awful.key({ modkey, "Shift"   }, "a", function () awful.util.spawn("matlab -desktop") end),
