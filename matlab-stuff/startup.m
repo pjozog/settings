1f686e7c9106222d10cec76966341b63a5e2cbc2
Paul Ozog <devnull@localhost>
2012-05-22 10:37:34 -0400

    Fixed breakpoints in matlab scripts - startup.m should use real path (ie - no paths with symbolic links)

--- matlab-stuff/startup.m
+++ matlab-stuff/startup.m
@@ -1,9 +1,9 @@
-run('~/Documents/MATLAB/vlfeat-0.9.13/toolbox/vl_setup.m');
+run('~/documents/MATLAB/vlfeat-0.9.13/toolbox/vl_setup.m');
 addpath(genpath('~/perl/perl-svn/perls/matlab/van'));
 addpath('~/perl/perl-svn/perls/matlab/');
-addpath('~/Documents/MATLAB/sift');
-addpath('~/Documents/MATLAB/toolbox_calib');
-addpath('~/Documents/MATLAB/misc');
+addpath('~/documents/MATLAB/sift');
+addpath('~/documents/MATLAB/toolbox_calib');
+addpath('~/documents/MATLAB/misc');
 addpath('~/perl/perl-svn/sba-eval/matlab/kuka');
 addpath('~/perl/perl-svn/sba-eval/matlab/utils');
 addpath('~/perl/perl-svn/sba-eval/matlab/kuka/postProcess');
