24378401a1cd77e7d316adfe33a5a3d3befffa2e
Paul Ozog <devnull@localhost>
2012-08-06 21:07:51 -0400

    fixed typo when launching chromium-browser

--- awesome-stuff/awesome/rc.lua
+++ awesome-stuff/awesome/rc.lua
@@ -205,7 +205,7 @@ globalkeys = awful.util.table.join(
    awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
    awful.key({ modkey, "Shift"   }, "y", function () awful.util.spawn("mypython") end),
    awful.key({ modkey, "Shift"   }, "f", function () awful.util.spawn("firefox") end),
-   awful.key({ modkey, "Shift"   }, "g", function () awful.util.spawn("chromium") end),
+   awful.key({ modkey, "Shift"   }, "g", function () awful.util.spawn("chromium-browser") end),
    awful.key({ modkey, "Shift"   }, "e", function () awful.util.spawn("emacs --debug-init") end),
    awful.key({ modkey, "Shift"   }, "m", function () awful.util.spawn("nautilus --no-desktop") end),
    awful.key({ modkey, "Shift"   }, "a", function () awful.util.spawn("matlab -desktop") end),
