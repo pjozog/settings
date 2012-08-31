8d2420c1cded18f2a47c6af5730da4b0fcc1a31f
Paul Ozog <devnull@localhost>
2012-08-31 08:55:23 -0400

    Added rule to make keepassx floating

--- awesome-stuff/awesome/rc.lua
+++ awesome-stuff/awesome/rc.lua
@@ -386,6 +386,8 @@ awful.rules.rules = {
      properties = { floating = true } },
    { rule = { name = "Ediff" },
      properties = { floating = true } },
+   { rule = { name = "keepassx" },
+     properties = { floating = true } },
    -- Set Firefox to always map on tags number 2 of screen 1.
    -- { rule = { class = "Firefox" },
    --   properties = { tag = tags[1][2] } },
