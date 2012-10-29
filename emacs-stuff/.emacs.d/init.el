81b232e68b305203a02373809b3e339d1c98da94
Paul Ozog <devnull@localhost>
2012-10-29 09:38:06 -0400

    C-M-o enters occur-mode. Minor changes to proposal. Redirect stdout to /dev/null when compiling.

--- emacs-stuff/.emacs.d/init.el
+++ emacs-stuff/.emacs.d/init.el
@@ -334,7 +334,7 @@
 (load "preview-latex.el" nil t t)
 
 ;; Activate occur easily inside isearch
-(define-key isearch-mode-map (kbd "C-o")
+(define-key isearch-mode-map (kbd "C-M-o")
   (lambda () (interactive)
     (let ((case-fold-search isearch-case-fold-search))
       (occur (if isearch-regexp isearch-string (regexp-quote isearch-string))))))
